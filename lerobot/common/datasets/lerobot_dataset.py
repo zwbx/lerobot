b39a2a20d79756a9b523d488ef5b736e433be7c3
wenbo zhang <38418735+zwbx@users.noreply.github.com>
2025-08-23 17:53:02 +0800

    Update lerobot_dataset.py
    Signed-off-by: wenbo zhang <38418735+zwbx@users.noreply.github.com>

--- lerobot/common/datasets/lerobot_dataset.py
+++ lerobot/common/datasets/lerobot_dataset.py
@@ -505,8 +505,10 @@ class LeRobotDataset(torch.utils.data.Dataset):
         self.episode_data_index = get_episode_data_index(self.meta.episodes, self.episodes)
 
         # Check timestamps
-        timestamps = torch.stack(self.hf_dataset["timestamp"]).numpy()
-        episode_indices = torch.stack(self.hf_dataset["episode_index"]).numpy()
+        # timestamps = torch.stack(self.hf_dataset["timestamp"]).numpy()
+        # episode_indices = torch.stack(self.hf_dataset["episode_index"]).numpy()
+        timestamps = torch.stack([torch.tensor(t) for t in self.hf_dataset["timestamp"]]).numpy()
+        episode_indices = torch.stack([torch.tensor(t) for t in self.hf_dataset["episode_index"]]).numpy()
         ep_data_index_np = {k: t.numpy() for k, t in self.episode_data_index.items()}
         check_timestamps_sync(timestamps, episode_indices, ep_data_index_np, self.fps, self.tolerance_s)
 
